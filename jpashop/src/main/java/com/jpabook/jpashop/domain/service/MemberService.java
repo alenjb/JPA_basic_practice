eae32b5de411ccb655c0fe051032b02ee188116f
alenjb <alenjb@naver.com>
2024-04-19 09:03:06 +0900

    feat: JPA 기본 예제 회원 서비스 개발

---
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.jpabook.jpashop.domain.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // 단일 회원 조회
+    public Member findOne(Long id){
+        return memberRepository.findOne(id);
+    }
+
+}
